2bbde5cf6f2f6a6fc5b0ec5e77949f79e4a66d92
Vijay Shankar <vijayayush.ak15@gmail.com>
2023-03-07 15:10:44 +0530

    fetch and display data from tvmaze api. create reusable function fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,57 @@
 // import { Link } from 'react-router-dom';
-import {useState} from 'react';
+import { useState } from 'react';
+import { searchForShows } from './../api/tvMaze';
 
 const Home = () => {
-    const [ searchStr , setSearchStr ]=useState('');
-    
-    const onSearchStringChange = (env) => {
-     setSearchStr(env.target.value);
-    }
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
-    const onSearch = async(env) => {
+
+  const onSearchStringChange = env => {
+    setSearchStr(env.target.value);
+  };
+
+  const onSearch = async env => {
     env.preventDefault();
 
-   const response = await fetch(
-    `https://api.tvmaze.com/search/shows?q=${searchStr}`
-     );
-    const body = await response.json();
-     console.log(body);
-    // https://api.tvmaze.com/search/shows?q=girls
+    try{
+      setApiDataError(null);
+      const result = await searchForShows(searchStr);
+      setApiData(result);
     }
+    catch(error){
+      setApiDataError(error);
+    }
+  };
+
+  const renderApiData = () => {
+  if(apiDataError){
+    return <div>Error occured: {apiDataError.message}</div>
+  }
+
+    if(apiData){
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+
+   return null;
+  }
 
   return (
     <div>
-    
-    <form onSubmit={onSearch}>
-    <input type='text' value={searchStr} onChange={ onSearchStringChange }></input>
-    <button type="submit">search</button>
-    </form>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchStr}
+          onChange={onSearchStringChange}
+        ></input>
+        <button type="submit">search</button>
+      </form>
+      <div>
+      {renderApiData()}
+      </div>
     </div>
   );
 };
