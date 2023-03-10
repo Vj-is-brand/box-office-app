24fddb68da70269eaa150f99d9b4e824107efbc7
Vijay Shankar <vijayayush.ak15@gmail.com>
2023-03-11 00:30:58 +0530

    used useQuery on home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,30 +4,41 @@ import ActorGrid from '../components/actors/ActorGrid';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import { searchForPeople, searchForShows } from './../api/tvMaze';
+import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
   
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  // const [apiData, setApiData] = useState(null);
+  // const [apiDataError, setApiDataError] = useState(null);
 
- 
+  const [filter, setFilter] = useState(null)
+
+  const { data:apiData, error:apiDataError } = useQuery({
+      queryKey: ['search', filter],
+      queryFn: () => filter.searchOption === 'shows' ? searchForShows (filter.q): searchForPeople(filter.q),
+      // ⬇️ disabled as long as the filter is empty
+      enabled: !!filter,
+      refetchOnWindowFocus:false
+  });
+
+//  const { data }= useQuery({queryKey:['onSearch',q],queryFn: () => searchForShows(q)})
   const onSearch = async ({q,searchOption}) => {
+     setFilter({q,searchOption});
+    // try {
+    //   setApiDataError(null);
 
-    try {
-      setApiDataError(null);
-
-      let result;
-      if (searchOption === 'shows') {
-         result = await searchForShows(q);
-      } else {
-         result = await searchForPeople(q);
-      }
+    //   let result;
+    //   if (searchOption === 'shows') {
+    //      result = await searchForShows(q);
+    //   } else {
+    //      result = await searchForPeople(q);
+    //   }
       
-      setApiData(result);
+    //   setApiData(result);
 
-    } catch (error) {
-      setApiDataError(error);
-    }
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
   };
 
   const renderApiData = () => {
--- src/pages/shows.jsx
+++ src/pages/shows.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { searchShowInfo } from '../api/tvMaze';
 import { useQuery } from '@tanstack/react-query';
