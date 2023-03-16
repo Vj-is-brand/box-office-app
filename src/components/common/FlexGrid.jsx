ecdc33278c5783f79a7effb4b421fcfb11d50acb
Vijay Shankar <vijayayush.ak15@gmail.com>
2023-03-17 01:24:26 +0530

    added fade-in animation to FlexGrid. added scale animation to starBtn. used useRef hook for taking managing state of animation

--- src/components/common/FlexGrid.jsx
+++ src/components/common/FlexGrid.jsx
@@ -4,4 +4,16 @@ export const FlexGrid = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-`;
+  
+  animation: fadein 0.8s ease-in forwards;
+
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+`
--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../common/SearchCard';
 import { StarIcon } from '../common/StarIcon';
@@ -6,6 +7,22 @@ const ShowCard = ({ id, image, name, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + '.....'
     : 'No Description';
+
+   const starBtnRef = useRef();
+   
+   const onHandleClick = () => {
+    onStarMeClick(id);
+    const starBtnEl = starBtnRef.current;
+
+    if(!starBtnEl) return;
+
+    if(isStarred){
+      starBtnEl.classList.remove('animate');
+    }else{
+      starBtnEl.classList.add('animate');
+    }
+   };
+
   return (
     <SearchCard>
       <SearchImgWrapper>
@@ -18,8 +35,8 @@ const ShowCard = ({ id, image, name, summary, onStarMeClick, isStarred }) => {
         <a href={`/shows/${id}`} target="_blank" rel="noreferrer">
           Read more
         </a>
-        <StarBtn type="button" onClick={() => onStarMeClick(id)}>
-        <StarIcon active={isStarred}/>
+        <StarBtn ref={starBtnRef} type="button" onClick={onHandleClick}>
+          <StarIcon active={isStarred} />
           {/* {isStarred ? 'UNSTAR' : 'STAR'} */}
         </StarBtn>
       </ActionSection>
@@ -55,5 +72,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
